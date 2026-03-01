5684349e6ea72ce967006b4436a30104daccb45d
sabuj-xdev <sabujwebdev@gmail.com>
2026-03-01 14:06:17 +0600

    Added Greade section

---
+++ Grade.js
@@ -0,0 +1,20 @@
+var num ;
+num=74;
+if (num>=90){
+
+    console.log ("grad A", num) 
+}
+else if (num >=80){
+    console.log ("Grad B", num) 
+}
+else if (num>=70) {
+    console.log ("Grad C", num )
+}
+else if (num>=60)
+{
+    console.log ("Grad D ", num )
+
+}
+else {
+    console.log ("Result is fail", num )
+}
