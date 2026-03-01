09fff4590554845b5ec936e1141d2d80bc16e550
sabuj-xdev <sabujwebdev@gmail.com>
2026-03-01 14:42:08 +0600

    Added new Nasted section

---
+++ nasted.js
@@ -0,0 +1,21 @@
+var score = 53;
+if (score>=80)
+{
+    console.log (" You are my Friend ")
+    }
+    if (score>=85){
+        console.log ("So sweet Friend  lets go  we are go for lunch ")
+    } 
+
+    else if (score<=80 && score>=60){
+        console.log ('good luck next time ',score)
+    }
+   
+else if (score<=60 && score>=40)
+{
+    console.log ("my friend msg is unseen ",score)
+}
+ 
+ else {
+        console.log ("block your friend ")
+    }; 
